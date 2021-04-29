7c918fca5940688d87ac50165c7ef3334e30181b
VAlentin Caminos Martinez <valentincaminos12345@gmail.com>
2021-04-29 15:31:25 -0300

    cambios en form, css y se agrego archivo js con cambios

---
+++ js/script.js
@@ -0,0 +1,23 @@
+  let nodo = document.getElementById ("enviar");
+  nodo.addEventListener ("click", validarFormulario);
+
+  function validarFormulario () {
+     let elementoCaptcha = document.getElementById("captcha");
+    let example = "just example";
+    if (example == elementoCaptcha.value) {
+        formularioEnviado();
+    }
+    else {
+        let boton = document.getElementById("enviar");
+        boton.value = "reintentar";
+        boton.disable = false;
+    }
+}
+
+    function formularioEnviado (){
+       let boton = document.getElementById("enviar");
+        boton.value = "enviado";
+        boton.disable = true;
+        
+}
+  
