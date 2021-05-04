162ac04c36bdccfb0e22860318f0816a39e8f6a0
VAlentin Caminos Martinez <valentincaminos12345@gmail.com>
2021-05-04 16:09:23 -0300

    cambio de logo, el .js se movio a la carpeta js, se modifico footer

--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
-  let nodo = document.getElementById ("enviar");
-  nodo.addEventListener ("click", validarFormulario);
+function sortearValor1() {
+    let valor1 = (Math.random() * 10).toFixed(0);
+    document.getElementById("valor1").innerHTML = valor1;
+    return valor1;
+}
 
-  function validarFormulario () {
-     let elementoCaptcha = document.getElementById("inputCaptcha");
-    let example = "just example";
-    if (example == elementoCaptcha.value) {
-        formularioEnviado();
-    }
-    else {
-        let boton = document.getElementById("enviar");
-        boton.value = "reintentar";
-        boton.disable = false;
+function sortearValor2() {
+    let valor2 = (Math.random() * 10).toFixed(0);
+    document.getElementById("valor2").innerHTML = valor2;
+    return valor2;
+}
+
+document.getElementById("valor1").innerHTML = sortearValor1();
+document.getElementById("valor2").innerHTML = sortearValor2();
+document.getElementById("botonEnviar").addEventListener("click", validarFormulario);
+let warning = document.getElementById("reintentar");
+let enviado = document.getElementById("enviado");
+
+function validarFormulario() {
+    let inputUsuario = document.getElementById("inputCaptcha");
+    let valorUsuario = inputUsuario.value;
+    let suma = Number(valor1.innerHTML) + Number(valor2.innerHTML);
+    if (valorUsuario == suma) {
+        warning.innerHTML = "";
+        enviarFormulario();
+        enviado.classList.add = ("confirmacion");
+        enviado.innerHTML = "¡Gracias por su compra!";
+    } else {
+        warning.innerHTML = "Incorrecto. Por favor, reintentar.";
+        sortearValor2();
     }
 }
 
-    function formularioEnviado (){
-       let boton = document.getElementById("enviar");
-        boton.value = "enviado";
-        boton.disable = true;
-        
+function enviarFormulario() {
+    let boton = document.getElementById("botonEnviar").value = "Enviado";
+    boton.disabled = true
+    let inputDeshabilitado = document.getElementById("inputCaptcha");
+    inputDeshabilitado.disabled = true;
+
 }
-  
